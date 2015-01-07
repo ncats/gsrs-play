6cc726fc2bedb96f6b3d6d558a6fc4fef751897d
Rajarshi Guha <rajarshi.guha@gmail.com>
2015-01-07 10:06:45 -0500

    fixed merge conflict

--- app/ix/idg/controllers/IDGApp.java
+++ app/ix/idg/controllers/IDGApp.java
@@ -1,24 +1,27 @@
 package ix.idg.controllers;
 
-import java.io.*;
-import java.sql.*;
-import java.util.*;
-import java.net.*;
-import java.util.concurrent.Callable;
 
-import play.*;
-import play.cache.Cache;
-import play.data.*;
-import play.mvc.*;
-import play.libs.ws.*;
-
-import ix.core.models.*;
-import ix.idg.models.*;
 import ix.core.controllers.SearchFactory;
+import ix.core.models.Text;
+import ix.core.models.Value;
+import ix.core.models.XRef;
 import ix.core.search.TextIndexer;
-
-import ix.utils.Global;
+import ix.idg.models.Disease;
+import ix.idg.models.Target;
 import ix.utils.Util;
+import play.Logger;
+import play.cache.Cache;
+import play.libs.ws.WS;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 
 public class IDGApp extends Controller {
@@ -148,7 +151,7 @@ public class IDGApp extends Controller {
 			uri.append(me.getKey()+"="+v+"&");
 	}
 	Logger.debug(">> "+uri);
-	return uri.substring(0, uri.length()-1);
+	return uri.substring(0, uri.length() - 1);
     }
 
     public static boolean hasFacet (TextIndexer.Facet facet, int i) {
@@ -280,7 +283,48 @@ public class IDGApp extends Controller {
 	}
     }
 
-    public static Result diseases () {
-	return ok (ix.idg.views.html.diseases.render());
-    }
+	public static Result disease(long id) {
+		try {
+			Disease d = DiseaseFactory.getDisease(id);
+			for (XRef xref : d.links) {
+				System.out.println(xref.refid + "/" + xref.kind + "/" + xref.deRef());
+				List<Value> props = xref.properties;
+				for (Value prop: props) {
+					System.out.println("prop = " + prop);
+					if (prop.getValue() instanceof Text) {
+						Text text = (Text) prop.getValue();
+						System.out.println("\t"+text.getText()+"/"+text.getValue());
+					}
+				}
+				System.out.println();
+			}
+			return DiseaseFactory.create();
+//			return ok(ix.idg.views.html.diseasedetails.render(t));
+		} catch (Exception ex) {
+			return internalServerError
+					(ix.idg.views.html.error.render(500, "Internal server error"));
+		}
+	}
+
+	public static Result diseases(int rows, int page) throws Exception {
+		TextIndexer.Facet[] facets = new TextIndexer.Facet[]{};
+//		TextIndexer.Facet[] facets = Cache.getOrElse("DiseaseFacets", new Callable<TextIndexer.Facet[]>() {
+//			public TextIndexer.Facet[] call() {
+//				return filter(getFacets(Disease.class, 20),
+//						"IDG Classification",
+//						"IDG Target Family"
+//						//"MeSH",
+//						//"Keyword"
+//				);
+//			}
+//		}, 3600);
+		int total = DiseaseFactory.finder.findRowCount();
+		rows = Math.min(total, Math.max(1, rows));
+		int[] pages = paging(rows, page, total);
+
+		List<Disease> diseases =
+				DiseaseFactory.getDiseases(rows, (page - 1) * rows, null);
+
+		return ok(ix.idg.views.html.diseases.render(page, rows, pages, facets, diseases));
+	}
 }
