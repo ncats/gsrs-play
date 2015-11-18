8def73ad5e00a9cdc28481a57f5eb5e3a4f882bd
Peryea <peryeata@NCATS-1907788-M.local>
2015-11-18 10:43:16 -0500

    Added relationship facet

--- modules/ginas/app/ix/ginas/controllers/GinasApp.java
+++ modules/ginas/app/ix/ginas/controllers/GinasApp.java
@@ -97,6 +97,7 @@ public class GinasApp extends App {
         "SubstanceStereoChemistry", 
         "Molecular Weight",
         "GInAS Tag", 
+        "Relationships",
         "Sequence Type", 
         "Material Class", 
         "Material Type",
@@ -270,6 +271,21 @@ public class GinasApp extends App {
             //final String name = super.name();
             if ("StructurallyDiverse".equalsIgnoreCase(label))
                 return "Structurally Diverse";
+            if ("protein".equalsIgnoreCase(label))
+                return "Protein";
+            if ("nucleicAcid".equalsIgnoreCase(label))
+                return "Nucleic Acid";
+            if ("polymer".equalsIgnoreCase(label))
+                return "Polymer";
+            if ("mixture".equalsIgnoreCase(label))
+                return "Mixture";
+            if ("chemical".equalsIgnoreCase(label))
+                return "Chemical";
+            if ("concept".equalsIgnoreCase(label))
+                return "Concept";
+            if (label.contains("->"))
+                return label.split("->")[1] + " of " + label.split("->")[0];
+            
             if ("EP".equalsIgnoreCase(label))
                 return "PH. EUR";
             if("approved".equalsIgnoreCase(label))
