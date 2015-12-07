0fe4f1343cea2ec09411406451bfe03b07dc29ff
Tyler Peryea <peryeata@cezanne.ncats.nih.gov>
2015-12-07 16:50:05 -0500

    changed serialization of references to set

--- app/ix/ginas/models/v1/ReferenceListSerializer.java
+++ app/ix/ginas/models/v1/ReferenceListSerializer.java
@@ -5,21 +5,21 @@ import ix.core.models.Value;
 import ix.ginas.models.GinasSubData;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-public class ReferenceListSerializer extends JsonSerializer<List<Value>> {
+public class ReferenceListSerializer extends JsonSerializer<Set<Value>> {
     public ReferenceListSerializer () {}
 
-    public void serialize (List<Value> list, JsonGenerator jgen,
+    public void serialize (Set<Value> list, JsonGenerator jgen,
                            SerializerProvider provider)
         throws IOException, JsonProcessingException {
-        List<String> refs = new ArrayList<String>();
+        Set<String> refs = new LinkedHashSet<String>();
         for (Value val : list) {
             if (GinasSubData.REFERENCE.equals(val.label)) {
                 Keyword kw = (Keyword)val;
