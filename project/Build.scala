a66e676678fba2f2a0ae6218cefafabd86672989
Rajarshi Guha <rajarshi.guha@gmail.com>
2016-01-11 14:42:26 -0500

    got rid of unmanaged JAI jars and updated build dependency on JAI

--- project/Build.scala
+++ project/Build.scala
@@ -172,6 +172,7 @@ public class BuildInfo {
     .enablePlugins(PlayJava).settings(commonSettings:_*).settings(
       libraryDependencies ++= commonDependencies,
       libraryDependencies += "org.apache.poi" % "poi" % "3.13",
+      libraryDependencies += "javax.media" % "jai_core" % "1.1.3",
         javacOptions ++= javaBuildOptions
     ).dependsOn(ncats).aggregate(ncats)
 
