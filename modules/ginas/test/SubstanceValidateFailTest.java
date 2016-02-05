295e877d342dd17ebff9d3ec587b658a74f3983c
Tyler Peryea <peryeata@cezanne.ncats.nih.gov>
2016-02-05 16:58:25 -0500

    further debugging jenkins

--- modules/ginas/test/SubstanceValidateFailTest.java
+++ modules/ginas/test/SubstanceValidateFailTest.java
@@ -7,11 +7,16 @@ import static play.test.Helpers.testServer;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 import org.junit.AfterClass;
 import org.junit.Test;
@@ -39,8 +44,14 @@ public class SubstanceValidateFailTest extends WithApplication {
     	
     	File folder=null;
     	try{
+    		for(String s:ResourceList.getResources(Pattern.compile(".*testJSON.*json"))){
+    			System.out.println("Hey! I found this:" + s);
+    		}
+    		
     		folder = new File(SubstanceValidateFailTest.class.getResource("testJSON/fail").getFile());
+    		
     	}catch(Exception e){
+    		
     		e.printStackTrace();
     		throw new IllegalStateException(e);
     	}
@@ -139,7 +150,112 @@ public class SubstanceValidateFailTest extends WithApplication {
         }
     });
     }*/
+    public static class ResourceList{
 
+        /**
+         * for all elements of java.class.path get a Collection of resources Pattern
+         * pattern = Pattern.compile(".*"); gets all resources
+         * 
+         * @param pattern
+         *            the pattern to match
+         * @return the resources in the order they are found
+         */
+        public static Collection<String> getResources(
+            final Pattern pattern){
+            final ArrayList<String> retval = new ArrayList<String>();
+            final String classPath = System.getProperty("java.class.path", ".");
+            final String[] classPathElements = classPath.split(System.getProperty("path.separator"));
+            for(final String element : classPathElements){
+                retval.addAll(getResources(element, pattern));
+            }
+            return retval;
+        }
+
+        private static Collection<String> getResources(
+            final String element,
+            final Pattern pattern){
+            final ArrayList<String> retval = new ArrayList<String>();
+            final File file = new File(element);
+            if(file.isDirectory()){
+                retval.addAll(getResourcesFromDirectory(file, pattern));
+            } else{
+                retval.addAll(getResourcesFromJarFile(file, pattern));
+            }
+            return retval;
+        }
+
+        private static Collection<String> getResourcesFromJarFile(
+            final File file,
+            final Pattern pattern){
+            final ArrayList<String> retval = new ArrayList<String>();
+            ZipFile zf;
+            try{
+                zf = new ZipFile(file);
+            } catch(final ZipException e){
+                throw new Error(e);
+            } catch(final IOException e){
+                throw new Error(e);
+            }
+            final Enumeration e = zf.entries();
+            while(e.hasMoreElements()){
+                final ZipEntry ze = (ZipEntry) e.nextElement();
+                final String fileName = ze.getName();
+                final boolean accept = pattern.matcher(fileName).matches();
+                if(accept){
+                    retval.add(fileName);
+                }
+            }
+            try{
+                zf.close();
+            } catch(final IOException e1){
+                throw new Error(e1);
+            }
+            return retval;
+        }
+
+        private static Collection<String> getResourcesFromDirectory(
+            final File directory,
+            final Pattern pattern){
+            final ArrayList<String> retval = new ArrayList<String>();
+            final File[] fileList = directory.listFiles();
+            for(final File file : fileList){
+                if(file.isDirectory()){
+                    retval.addAll(getResourcesFromDirectory(file, pattern));
+                } else{
+                    try{
+                        final String fileName = file.getCanonicalPath();
+                        final boolean accept = pattern.matcher(fileName).matches();
+                        if(accept){
+                            retval.add(fileName);
+                        }
+                    } catch(final IOException e){
+                        throw new Error(e);
+                    }
+                }
+            }
+            return retval;
+        }
+
+        /**
+         * list the resources that match args[0]
+         * 
+         * @param args
+         *            args[0] is the pattern to match, or list all resources if
+         *            there are no args
+         */
+        public static void main(final String[] args){
+            Pattern pattern;
+            if(args.length < 1){
+                pattern = Pattern.compile(".*");
+            } else{
+                pattern = Pattern.compile(args[0]);
+            }
+            final Collection<String> list = ResourceList.getResources(pattern);
+            for(final String name : list){
+                System.out.println(name);
+            }
+        }
+    }  
     @AfterClass
     public static void tearDown(){
         // Stop the server
