7deadd8a35e71df5581e6badbca965c82bb8eb11
katzelda <daniel.katzel@nih.gov>
2016-02-17 09:37:04 -0500

    updated deploy to point to correct jenkins path

--- deploy.pl
+++ deploy.pl
@@ -13,8 +13,8 @@ my @files = glob('modules/ginas/target/universal/*.zip');
 my $zipFile = $files[0];
 
 my $zipName = basename($zipFile);
-#my $outputPath = "/ncats/users/ncatsweb/www/files/ginastmp/$zipName";
-my $outputPath = "ginastmp/$zipName";
+my $outputPath = "/ncats/users/ncatsweb/www/files/ginastmp/$zipName";
+#my $outputPath = "ginastmp/$zipName";
 
 copy($zipFile, $outputPath)  or die "Copy failed: $!";
 
@@ -27,6 +27,6 @@ my $abs_path = abs_path($dir. "/$subDir");
 chdir ($abs_path) or die  "cannot change to $abs_path: $!\n";
 print("working dir is $abs_path\n");
 
-my $command = "bin/ginas -Djava.awt.headless=true -Dhttp.port=9004 -Dconfig.resource=conf/ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
+my $command = "./bin/ginas -Djava.awt.headless=true -Dhttp.port=9004 -Dconfig.resource=conf/ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
 
 system($command. " &");
