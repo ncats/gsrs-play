108ce3de04c87063430807d397dfce091551bd65
katzelda <daniel.katzel@nih.gov>
2016-02-19 10:09:50 -0500

    added explicit call to perl incase of path issue.

--- deploy.pl
+++ deploy.pl
@@ -47,7 +47,7 @@ my $abs_path = abs_path($outputPath. "/$subDir");
 chdir ($abs_path) or die  "cannot change to $abs_path: $!\n";
 print("working dir is $abs_path\n");
 
-my $command = "$abs_path/bin/ginas -Djava.awt.headless=true -Dhttp.port=9003 -Dconfig.resource=ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
+my $command = "/usr/bin/perl $abs_path/bin/ginas -Djava.awt.headless=true -Dhttp.port=9003 -Dconfig.resource=ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
 
 my $daemon = Proc::Daemon->new(
         work_dir => $abs_path,
