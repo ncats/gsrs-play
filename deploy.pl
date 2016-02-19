90521ace1db8f5c402611f8e1f1242f53914a6d8
katzelda <daniel.katzel@nih.gov>
2016-02-19 10:34:25 -0500

    changed back to 9003 and added username printout

--- deploy.pl
+++ deploy.pl
@@ -17,6 +17,8 @@ my @files = glob('modules/ginas/target/universal/*.zip');
 #should only be one file
 my $zipFile = $files[0];
 
+print "Running as: ", getpwuid($<), "\n";
+
 my $zipName = basename($zipFile);
 my $outputPath = "/ncats/users/ncatsweb/www/files/ginastmp/currentDevApp";
 #my $outputPath = "ginastmp/currentDevApp";
@@ -47,7 +49,7 @@ my $abs_path = abs_path($outputPath. "/$subDir");
 chdir ($abs_path) or die  "cannot change to $abs_path: $!\n";
 print("working dir is $abs_path\n");
 
-my $command = "/usr/bin/perl $abs_path/bin/ginas -Djava.awt.headless=true -Dhttp.port=9004 -Dconfig.resource=ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
+my $command = "/usr/bin/perl $abs_path/bin/ginas -Djava.awt.headless=true -Dhttp.port=9003 -Dconfig.resource=ginas.conf -DapplyEvolutions.default=true -Dapplication.context=/dev/ginas/app";
 
 my $daemon = Proc::Daemon->new(
         work_dir => $abs_path,
