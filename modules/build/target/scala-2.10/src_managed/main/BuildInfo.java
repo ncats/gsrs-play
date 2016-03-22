bbff8c4fb52f4ab147511f36f3b5a9a18ae10e57
Peryea <peryeata@NCATS-1907788-M.local>
2016-03-22 18:01:34 -0400

    removed target files

--- modules/build/target/scala-2.10/src_managed/main/BuildInfo.java
+++
@@ -1,9 +0,0 @@
-
-package ix;
-public class BuildInfo { 
-   public static final String BRANCH = "master";
-   public static final String DATE = "20160322";
-   public static final String COMMIT = "d8f1b0f";
-   public static final String TIME = "Tue Mar 22 15:36:29 EDT 2016";
-   public static final String AUTHOR = "Danny Katzel";
-}
