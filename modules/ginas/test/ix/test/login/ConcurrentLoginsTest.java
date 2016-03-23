8a713587cff02f1ef10e2e4f30340105f1bfb00f
Danny Katzel <daniel.katzel@nih.gov>
2016-03-23 15:13:44 -0400

    even more user login tests

--- modules/ginas/test/ix/test/login/ConcurrentLoginsTest.java
+++ modules/ginas/test/ix/test/login/ConcurrentLoginsTest.java
@@ -1,21 +1,51 @@
 package ix.test.login;
 
+import chemaxon.nfunk.jep.function.Abs;
+import ix.test.ix.test.server.AbstractSession;
 import ix.test.ix.test.server.GinasTestServer;
 import ix.test.ix.test.server.RestSession;
 import ix.test.util.MultiThreadInteracter;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static ix.test.login.LoginUtil.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by katzelda on 3/23/16.
  */
+@RunWith(Parameterized.class)
 public class ConcurrentLoginsTest {
 
     @Rule
     public GinasTestServer ts = new GinasTestServer(9001);
+
+
+    @Parameterized.Parameters
+    public static List<Object[]> params(){
+
+        List<Object[]> list = new ArrayList<>();
+        for(LoginStrategy s : LoginStrategy.values()){
+
+            for(LoginStrategy s2 : LoginStrategy.values()){
+                    list.add(new Object[]{s, s2});
+                }
+            }
+
+        return list;
+    }
+
+    private LoginStrategy strategy, strategy2;
+
+    public ConcurrentLoginsTest(LoginStrategy strategy, LoginStrategy strategy2){
+        this.strategy = strategy;
+        this.strategy2 = strategy2;
+    }
 
     @Test
     public void twoDifferentLoggedInUsersViewSubstancesMultiThreadedInterleaved() {
@@ -23,8 +53,8 @@ public class ConcurrentLoginsTest {
         final GinasTestServer.User user1 = ts.getFakeUser1();
         final GinasTestServer.User user3 = ts.getFakeUser3();
 
-        try (final RestSession session1 = ts.newRestSession(user1);
-             final RestSession session3 = ts.newRestSession(user3);) {
+        try (final AbstractSession session1 = strategy.loginAs(ts, user1);
+             final AbstractSession session3 = strategy2.loginAs(ts, user3);) {
 
             MultiThreadInteracter.Builder builder = new MultiThreadInteracter.Builder();
 
@@ -75,8 +105,8 @@ public class ConcurrentLoginsTest {
         final GinasTestServer.User user1 = ts.getFakeUser1();
         final GinasTestServer.User user3 = ts.getFakeUser3();
 
-        try (final RestSession session1 = ts.newRestSession(user1);
-             final RestSession session3 = ts.newRestSession(user3);) {
+        try (final AbstractSession session1 = strategy.loginAs(ts, user1);
+             final AbstractSession session3 = strategy2.loginAs(ts, user3);) {
 
             MultiThreadInteracter.Builder builder = new MultiThreadInteracter.Builder();
 
@@ -128,8 +158,8 @@ public class ConcurrentLoginsTest {
 
         final GinasTestServer.User user1 = ts.getFakeUser1();
 
-        try (final RestSession session1 = ts.newRestSession(user1);
-             final RestSession session3 = ts.notLoggedInRestSession();) {
+        try (final AbstractSession session1 = strategy.loginAs(ts, user1);
+             final AbstractSession session3 = strategy2.getNotLoggedIn(ts)) {
 
             MultiThreadInteracter.Builder builder = new MultiThreadInteracter.Builder();
 
@@ -189,12 +219,12 @@ public class ConcurrentLoginsTest {
         final Random rand = new Random();
         for(int i=0; i< numLoggedInUsers; i++){
             final GinasTestServer.User user = ts.createNormalUser("testUser"+i, "password" +i);
-            final RestSession[] session = new RestSession[1];
+            final AbstractSession[] session = new AbstractSession[1];
             builder.newThread()
                     .step(0, new MultiThreadInteracter.Step() {
                         @Override
                         public void execute() throws Exception {
-                            session[0] = ts.newRestSession(user);
+                            session[0] = strategy.loginAs(ts, user);
                         }
                     })
                     .step(rand.nextInt(2) + 1, new MultiThreadInteracter.Step(){
@@ -223,12 +253,12 @@ public class ConcurrentLoginsTest {
 
         for(int i=0; i< numNotLoggedInUsers; i++){
 
-            final RestSession[] session = new RestSession[1];
+            final AbstractSession[] session = new AbstractSession[1];
             builder.newThread()
                     .step(0, new MultiThreadInteracter.Step() {
                         @Override
                         public void execute() throws Exception {
-                            session[0] = ts.notLoggedInRestSession();
+                            session[0] = strategy2.getNotLoggedIn(ts);
                         }
                     })
                     .step(rand.nextInt(2) + 1, new MultiThreadInteracter.Step(){
@@ -253,5 +283,57 @@ public class ConcurrentLoginsTest {
 
         multiThreadInteracter.run();
 
+    }
+
+
+    private enum LoginStrategy{
+        BROWSER{
+            @Override
+            public AbstractSession<?> loginAs(GinasTestServer ts, GinasTestServer.User user) {
+                return ts.newBrowserSession(user);
+            }
+
+            @Override
+            public AbstractSession<?> getNotLoggedIn(GinasTestServer ts) {
+                return ts.notLoggedInBrowserSession();
+            }
+        },
+        API_TOKEN{
+            @Override
+            public AbstractSession<?> loginAs(GinasTestServer ts, GinasTestServer.User user) {
+                return ts.newRestSession(user, RestSession.AUTH_TYPE.TOKEN);
+            }
+
+            @Override
+            public AbstractSession<?> getNotLoggedIn(GinasTestServer ts) {
+                return ts.notLoggedInRestSession();
+            }
+        },
+        API_USER_PASS{
+            @Override
+            public AbstractSession<?> loginAs(GinasTestServer ts, GinasTestServer.User user) {
+                return ts.newRestSession(user, RestSession.AUTH_TYPE.USERNAME_PASSWORD);
+            }
+
+            @Override
+            public AbstractSession<?> getNotLoggedIn(GinasTestServer ts) {
+                return ts.notLoggedInRestSession();
+            }
+        },
+        API_KEY{
+            @Override
+            public AbstractSession<?> loginAs(GinasTestServer ts, GinasTestServer.User user) {
+                return ts.newRestSession(user, RestSession.AUTH_TYPE.USERNAME_KEY);
+            }
+
+            @Override
+            public AbstractSession<?> getNotLoggedIn(GinasTestServer ts) {
+                return ts.notLoggedInRestSession();
+            }
+        };
+
+        public abstract AbstractSession<?> loginAs(GinasTestServer ts, GinasTestServer.User user);
+
+        public abstract AbstractSession<?> getNotLoggedIn(GinasTestServer ts);
     }
 }
