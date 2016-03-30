2667615018a18db31767fb3e18348112f4431e70
Peryea <peryeata@NCATS-1907788-M.home>
2016-03-29 23:49:10 -0400

    fixed some normalizing on profiler

--- app/ix/utils/TimeProfiler.java
+++ app/ix/utils/TimeProfiler.java
@@ -83,8 +83,10 @@ public class TimeProfiler{
 	    			ltimes= new ArrayList<Double>();
 	    			lastTimes.put(key,ltimes);
 	    		}
+	    		
 	    		ltimes.add(avgTime);
-	    		normalizeTimes(ltimes, chunkSize);
+	    		if(ltimes.size()>chunkSize)
+	    			normalizeTimes(ltimes, chunkSize);
 	    		lastChunkCount.reset(key);
 	    		lastChunkTimeCount.reset(key);
     		}
@@ -120,21 +122,22 @@ public class TimeProfiler{
 			}
     	}
     	
-    	private void normalizeTimes(List<Double> times, int samples){
+    	private static void normalizeTimes(List<Double> times, int samples){
     		double[] mytimes=new double[samples];
     		for(int i=0;i<samples;i++){
-    			mytimes[i]=interp(times,(i+0.5)/(double)samples);
+    			mytimes[i]=interp(times,(i+0)/((double)samples));
     		}
     		times.clear();
     		for(int i=0;i<samples;i++){
     			times.add(mytimes[i]);
     		}
-    		
     	}
-    	private double interp(List<Double> times, double t){
-    		double approx=t*times.size();
+    	private static double interp(List<Double> times, double t){
+    		double approx=t*(times.size());
+    		
     		int low = Math.max(0,(int)Math.floor(approx));
     		int high = Math.min((int)Math.ceil(approx), times.size()-1);
+    		
     		double additional = approx-low;
     		double d1=times.get(low);
     		double d2=times.get(high);
@@ -142,27 +145,38 @@ public class TimeProfiler{
     		
     	}
     	
+    	public void printResult(String s){
+
+			double average=getAverageTime(s,TimeUnit.MILLISECONDS);
+			double lastAverage = getLastAverageTime(s,TimeUnit.MILLISECONDS);
+			
+			if(timeCount.count.containsKey(s)){
+				StringBuilder sb = new StringBuilder();
+				List<Double> times=new ArrayList<Double>(lastTimes.get(s));
+				if(times.size()<this.chunkSize){
+					normalizeTimes(times, this.chunkSize);
+				}
+				int i=0;
+    			for(double avg:times){
+    				if(i>0){
+    					sb.append("\t" + avg/(1E6+0.0));
+    				}
+    				i++;
+    			}
+    			System.out.println(
+    					s + "\t" + 
+    					numberCount.get(s) + "\t" + 
+    					(timeCount.get(s)/(1E6+0.0)) + "\t" +
+    					average + sb.toString() + "\t" + lastAverage);
+			}else{
+				//System.out.println("NO key:" + s + " avg:" + average);
+			}
+    	}
+    	
     	public void printResults(){
     		System.out.println("===========");
     		for(String s:numberCount.getKeys()){
-    			
-    			double average=getAverageTime(s,TimeUnit.MILLISECONDS);
-    			double lastAverage = getLastAverageTime(s,TimeUnit.MILLISECONDS);
-    			
-    			if(timeCount.count.containsKey(s)){
-    				StringBuilder sb = new StringBuilder();
-	    			
-	    			for(double avg:lastTimes.get(s)){
-	    				sb.append("\t" + avg/(1E6+0.0));
-	    			}
-	    			System.out.println(
-	    					s + "\t" + 
-	    					numberCount.get(s) + "\t" + 
-	    					(timeCount.get(s)/(1E6+0.0)) + "\t" +
-	    					average + sb.toString() + "\t" + lastAverage);
-    			}else{
-    				//System.out.println("NO key:" + s + " avg:" + average);
-    			}
+    			printResult(s);
     		}
     		System.out.println("==========");
     	}
