5399d51f0e535db6aa1d2076f888680f660766ae
Danny Katzel <daniel.katzel@nih.gov>
2016-04-13 10:50:00 -0400

    refactored reindex tests to use base abstract class for re-usable components. #567

--- modules/ginas/test/ix/test/load/LoadDataSetTest.java
+++ modules/ginas/test/ix/test/load/LoadDataSetTest.java
@@ -22,33 +22,11 @@ import static org.junit.Assert.*;
 /**
  * Created by katzelda on 4/4/16.
  */
-public class LoadDataSetTest {
+public class LoadDataSetTest extends AbstractLoadDataSetTest{
 
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
-
-    private GinasTestServer ts = new GinasTestServer();
-
-    @Rule
-    public RuleChain chain = RuleChain.outerRule( new TestNamePrinter())
-                                                    .around(ts);
-
-    private GinasTestServer.User admin;
-
-    @Before
-    public void createAdmin(){
-        admin = ts.createAdmin("admin2", "adminPass");
-    }
-
-
-    public static void assertFacetsMatch(Map<String, Map<String, Integer>> expectedFacets, SubstanceSearch.SearchResult actualResults){
-        Map<String, Map<String, Integer>> actual = actualResults.getAllFacets();
-
-
-        assertEquals(expectedFacets, actual);
-    }
-
 
     @Test
     public void loadMultipleFiles() throws IOException {
@@ -73,59 +51,6 @@ public class LoadDataSetTest {
         }
     }
 
-    private Map<String, Map<String, Integer>> createExpectedRep90Facets() {
-        Map<String, Map<String, Integer>> expectedFacets = new HashMap<>();
-
-        expectedFacets.put("Record Status", new HashMap<String, Integer>(){{
-                                                        put("Validated (UNII)", 17);
-                                                    }});
-        expectedFacets.put("Substance Class", new HashMap<String, Integer>(){{
-                    put("Chemical", 17);
-                }});
-        expectedFacets.put("Molecular Weight", new HashMap<String, Integer>(){{
-                    put("200:400", 10);
-                    put("0:200", 9);
-                    put("400:600", 2);
-                    put(">1000", 2);
-                    put("800:1000", 1);
-                }});
-        expectedFacets.put( "GInAS Tag", new HashMap<String, Integer>(){{
-                    put("NOMEN", 17);
-                    put("WARNING", 17);
-                    put("WHO-DD", 6);
-                    put("MI", 6);
-                    put("INCI", 2);
-                    put("INN", 1);
-                    put("HSDB", 1);
-                    put("MART.", 1);
-                    put("FCC", 1);
-                    put("FHFI", 1);
-                }});
-
-        //Stereochemistry={ACHIRAL=13, ABSOLUTE=3, RACEMIC=1},
-        expectedFacets.put("Stereochemistry", new HashMap<String, Integer>(){{
-
-                put("ACHIRAL", 13);
-                put("ABSOLUTE", 3);
-                put("RACEMIC", 1);
-            }});
-        //Structure Hash={NNQ793F142LD=5, 1GMA5YNPSNF6=1, 9QJCPY53NHZV=1, L6RUGLWCMMP4=1, NNQ1X6A91CVX=1, NV2AC53S72NK=1, PA437XKNCWR2=1, VU8BQZFPPYTZ=1, YPCZM11BTJ54=1, Z3T91W4NXAHP=1 }
-        expectedFacets.put("Structure Hash", new HashMap<String, Integer>(){{
-            put("NNQ793F142LD", 5);
-            put("1GMA5YNPSNF6", 1);
-            put("9QJCPY53NHZV", 1);
-            put("L6RUGLWCMMP4", 1);
-            put("NNQ1X6A91CVX", 1);
-            put("NV2AC53S72NK", 1);
-            put("PA437XKNCWR2", 1);
-            put("VU8BQZFPPYTZ", 1);
-            put("YPCZM11BTJ54", 1);
-            put("Z3T91W4NXAHP", 1);
-
-        }});
-
-            return expectedFacets;
-    }
 
     @Test
     public void loadAsAdmin() throws IOException {
