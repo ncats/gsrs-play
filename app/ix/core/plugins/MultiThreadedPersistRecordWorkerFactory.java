0843bfe2fe610ba18729c544d621177cce68f812
Danny Katzel <daniel.katzel@nih.gov>
2016-04-29 17:06:00 -0400

    removed most of the javadoc which was crashing scala / JVM ?

--- app/ix/core/plugins/MultiThreadedPersistRecordWorkerFactory.java
+++ app/ix/core/plugins/MultiThreadedPersistRecordWorkerFactory.java
@@ -3,17 +3,7 @@ package ix.core.plugins;
 /**
  * A PersistRecordWorkerFactory that can persist many records at the same time from different threads
  * concurrently.
- * <p>
- * This class had to be made in its own file instead of
- * being a static inner class in GinasRecordProcessPlugin because it has to be instantiated
- * by reflection and however Play framework enhances classes messes up inner classes package names
- * enough that Class.forName() can't find them.
- *</p>
- * <p>
- * To select this class as the factory implementation, set
- * {@code ix.ginas.PersistRecordWorkerFactoryImpl} value to this
- * fully qualified class name in the conf file.
- *</p>
+ *
  *
  *
  * Created by katzelda on 4/28/16.
--- app/ix/core/plugins/SingleThreadedPersistRecordWorkerFactory.java
+++ app/ix/core/plugins/SingleThreadedPersistRecordWorkerFactory.java
@@ -4,17 +4,7 @@ package ix.core.plugins;
  * A PersistRecordWorkerFactory that can persist only a single record at the same time even if in a multi-thread
  * enviornment.  Some database implementations can't save transactions across multiple threads so
  * this implementation should be used for them.
- *<p>
- * This class had to be made in its own file instead of
- * being a static inner class in GinasRecordProcessPlugin because it has to be instantiated
- * by reflection and however Play framework enhances classes messes up inner classes package names
- * enough that Class.forName() can't find them.
- *</p>
- * <p>
- * To select this class as the factory implementation, set
- * {@code ix.ginas.PersistRecordWorkerFactoryImpl} value to this
- * fully qualified class name in the conf file.
- *</p>
+ *
  *
  *
  * Created by katzelda on 4/28/16.
