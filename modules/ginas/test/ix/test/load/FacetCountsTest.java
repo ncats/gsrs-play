ae42b980bb18b983144c2aced9daab305392ad13
Danny Katzel <daniel.katzel@nih.gov>
2016-04-29 14:08:42 -0400

    added tests to make sure substructure search facets are updated after data loads #645

--- modules/ginas/test/ix/test/load/FacetCountsTest.java
+++ modules/ginas/test/ix/test/load/FacetCountsTest.java
@@ -2,11 +2,14 @@ package ix.test.load;
 
 import ix.test.ix.test.server.BrowserSession;
 import ix.test.ix.test.server.GinasTestServer;
+import ix.test.ix.test.server.SubstanceLoader;
 import ix.test.ix.test.server.SubstanceSearch;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -18,18 +21,67 @@ public class FacetCountsTest {
 
     @Rule
     public GinasTestServer ts = new GinasTestServer();
-    private Map<String, Map<String, Integer>> allFacets;
+
+
+
 
     @Test
-    public void facetCountsInitiallyZero() throws IOException {
-        try(BrowserSession session = ts.newBrowserSession(ts.getFakeUser1())){
+    public void facetCountsGetUpdatedWhenDataIsLoaded() throws IOException{
+        GinasTestServer.User admin = ts.createAdmin("admin2", "adminPass");
+
+        try(BrowserSession session = ts.newBrowserSession(admin)){
+
+            SubstanceLoader loader = new SubstanceLoader(session);
+
             SubstanceSearch searcher = new SubstanceSearch(session);
+
+            loader.loadJson(new File("test/testdumps/rep90_part1.ginas"));
 
             SubstanceSearch.SearchResult results = searcher.substructure("C1=CC=CC=C1");
 
-            assertEquals(0, results.numberOfResults());
-            System.out.println(allFacets);
-            assertTrue(allFacets.isEmpty());
+
+            assertEquals(9, results.numberOfResults());
+
+            Map<String, Map<String,Integer>> partialFacets = new HashMap<>();
+
+            partialFacets.put("Record Status", new HashMap<String,Integer>(){{
+                                                    put("Validated (UNII)", 9);
+            }});
+
+            partialFacets.put("Substance Type", new HashMap<String,Integer>(){{
+                put("Chemical", 9);
+            }});
+            partialFacets.put("Stereochemistry", new HashMap<String,Integer>(){{
+                put("ACHIRAL", 7);
+                put("ABSOLUTE", 1);
+                put("RACEMIC", 1);
+            }});
+
+            partialFacets.put("Molecular Weight", new HashMap<String,Integer>(){{
+                put("0:200", 5);
+                put("200:400", 3);
+                put("400:600", 1);
+                put("800:1000", 1);
+                put(">1000", 1);
+            }});
+
+            partialFacets.put("GInAS Tag", new HashMap<String,Integer>(){{
+                put("NOMEN", 9);
+                put("WARNING", 9);
+                put("MI", 2);
+                put("WHO-DD", 2);
+                put("INCI", 1);
+            }});
+
+
+            TestFacetUtil.assertFacetsMatch(partialFacets, results);
+
+            loader.loadJson(new File("test/testdumps/rep90_part2.ginas"));
+
+            SubstanceSearch.SearchResult results2 = searcher.substructure("C1=CC=CC=C1");
+
+
+            TestFacetUtil.assertFacetsMatch(TestFacetUtil.createExpectedRep90Facets(), results2);
         }
     }
 }
