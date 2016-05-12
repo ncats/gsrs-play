a5ede5ec47e100647313416d1ad7bbe691a9d879
Danny Katzel <daniel.katzel@nih.gov>
2016-05-12 16:35:41 -0400

    added StopWatch utility class.  Has to be in seqaln since that is depended on by "core" so this way all inxight classes can reference it...

---
+++ modules/seqaln/src/main/java/ix/core/util/StopWatch.java
@@ -0,0 +1,28 @@
+package ix.core.util;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by katzelda on 5/12/16.
+ */
+public final class StopWatch {
+
+    private StopWatch(){
+        //can not instantiate
+    }
+
+    public static long timeElaspsed(Runnable r){
+        long start = System.currentTimeMillis();
+        r.run();
+        long end = System.currentTimeMillis();
+        return end-start;
+
+    }
+
+    public static long timeElapsed(Callable<Void> c) throws Exception{
+        long start = System.currentTimeMillis();
+        c.call();
+        long end = System.currentTimeMillis();
+        return end-start;
+    }
+}
