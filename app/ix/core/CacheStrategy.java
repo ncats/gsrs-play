4454fe43554752a8404b60c0c2c660e1cb67f5c8
Danny Katzel <daniel.katzel@nih.gov>
2016-05-19 11:37:29 -0400

    added javadoc

--- app/ix/core/CacheStrategy.java
+++ app/ix/core/CacheStrategy.java
@@ -7,10 +7,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to put on classes of objects that
+ * get cached to explain
+ * the rules for how this object
+ * should be cached.
+ */
 @Documented
 @Retention(value=RetentionPolicy.RUNTIME)
 @Inherited
 @Target(value={ElementType.TYPE})
 public @interface CacheStrategy {
+	/**
+	 * Is this object allowed to be evicted from the
+	 * cache if the cache gets too big.
+	 * @return {@code true} if it can (the default)
+	 * and {@code false} if it should never be evicted.
+     */
 	 boolean evictable () default true;
 }
