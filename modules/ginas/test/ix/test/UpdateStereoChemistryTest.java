ac66dffbb82663de484b283c37f343f929cdd408
Mandava <mandavag@nih.gov>
2016-06-10 10:14:06 -0400

    unit tests for issue#809

---
+++ modules/ginas/test/ix/test/UpdateStereoChemistryTest.java
@@ -0,0 +1,106 @@
+package ix.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import ix.test.ix.test.server.GinasTestServer;
+import ix.test.ix.test.server.RestSession;
+import ix.test.ix.test.server.SubstanceAPI;
+import ix.test.util.TestNamePrinter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import util.json.JsonUtil;
+
+import java.io.File;
+
+import static ix.test.SubstanceJsonUtil.ensurePass;
+import static org.junit.Assert.assertEquals;
+
+public class UpdateStereoChemistryTest {
+    final File chemicalResource=new File("test/testJSON/editChemical.json");
+
+    @Rule
+    public TestNamePrinter printer = new TestNamePrinter();
+
+    @Rule
+    public GinasTestServer ts = new GinasTestServer(9001);
+
+
+    private GinasTestServer.User fakeUser1;
+
+    @Before
+    public void getUsers(){
+        fakeUser1 = ts.getFakeUser1();
+    }
+
+    @Test
+    public void testStereoChemistryUpdate() throws Exception {
+
+        RestSession session = ts.newRestSession(fakeUser1);
+
+        SubstanceAPI api = new SubstanceAPI(session);
+        JsonNode entered= SubstanceJsonUtil
+                .prepareUnapprovedPublic(JsonUtil.parseJsonFile(chemicalResource));
+        String uuid=entered.get("uuid").asText();
+        ensurePass(api.submitSubstance(entered));
+        JsonNode fetched=api.fetchSubstanceJsonByUuid(uuid);
+        JsonNode structure = fetched.at("/structure");
+        String stereoChem = structure.get("stereochemistry").asText();
+        String opticalAct = structure.get("opticalActivity").asText();
+        String atropisomerism = structure.get("atropisomerism").asText();
+        assertEquals(stereoChem, "ACHIRAL");
+        assertEquals(opticalAct, "NONE");
+        assertEquals(atropisomerism, "No");
+
+        JsonNode updated = new JsonUtil
+                .JsonNodeBuilder(fetched)
+                .set("/structure/stereochemistry", "EPIMERIC")
+                .set("/structure/opticalActivity", "( + )")
+                .set("/structure/atropisomerism", "Unknown")
+                .build();
+
+        ensurePass(api.updateSubstance(updated));
+        JsonNode fetchedagain=api.fetchSubstanceJsonByUuid(uuid);
+        JsonNode newStructure = fetchedagain.at("/structure");
+        String newStereochem = newStructure.get("stereochemistry").asText();
+        String newOptact = newStructure.get("opticalActivity").asText();
+        String newatropi = newStructure.get("atropisomerism").asText();
+        assertEquals(newStereochem, "EPIMERIC");
+        assertEquals(newOptact, "( + )");
+        assertEquals(newatropi, "Unknown");
+    }
+
+    @Test
+    public void testMoietyUpdate() throws Exception {
+
+        RestSession session = ts.newRestSession(fakeUser1);
+
+        SubstanceAPI api = new SubstanceAPI(session);
+        JsonNode entered= SubstanceJsonUtil
+                .prepareUnapprovedPublic(JsonUtil.parseJsonFile(chemicalResource));
+        String uuid=entered.get("uuid").asText();
+        ensurePass(api.submitSubstance(entered));
+        JsonNode fetched=api.fetchSubstanceJsonByUuid(uuid);
+        JsonNode moiety = fetched.at("/moieties/0");
+        String stereoChem = moiety.get("stereochemistry").asText();
+        String opticalAct = moiety.get("opticalActivity").asText();
+        assertEquals(stereoChem, "ACHIRAL");
+        assertEquals(opticalAct, "NONE");
+
+        JsonNode updated = new JsonUtil
+                .JsonNodeBuilder(fetched)
+                .set("/moieties/0/stereochemistry", "EPIMERIC")
+                .set("/moieties/0/opticalActivity", "( + )")
+                .add("/moieties/0/atropisomerism", "Unknown")
+                .build();
+
+        ensurePass(api.updateSubstance(updated));
+        JsonNode fetchedagain=api.fetchSubstanceJsonByUuid(uuid);
+        JsonNode newMoiety = fetchedagain.at("/moieties/0");
+        String newStereochem = newMoiety.get("stereochemistry").asText();
+        String newOptact = newMoiety.get("opticalActivity").asText();
+        String newatropi = newMoiety.get("atropisomerism").asText();
+        assertEquals(newStereochem, "EPIMERIC");
+        assertEquals(newOptact, "( + )");
+        assertEquals(newatropi, "Unknown");
+    }
+}
