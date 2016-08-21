a47987b2420f36e3321f59ae576c8178e1233a90
Peryea <peryeata@NCATS-1907788-M.lan>
2016-08-21 11:45:24 -0400

    added extra formula processor

--- app/ix/core/chem/Chem.java
+++ app/ix/core/chem/Chem.java
@@ -36,39 +36,17 @@ public class Chem {
     
     /**
      * Generate chemical formula by treating disconnected components
-     * seperately.
+     * separately.
      */
     public static String formula (Molecule g) {
         Molecule[] frags = g.cloneMolecule().convertToFrags();
         final Map<String, Integer> formula = new HashMap<String, Integer>();
         for (Molecule m : frags) {
-            for (MolAtom a : m.getAtomArray()) {
-                // get the right H count
-                int c = a.getCharge();
-                if (c < 0) {
-                    a.setCharge(0);
-                    a.setImplicitHcount(-c);
-                }
-            }
             String f = m.getFormula();
             Integer c = formula.get(f);
             formula.put(f, c==null ? 1 : (c+1));
         }
         List<String> order = new ArrayList<String>(formula.keySet());
-        Collections.sort(order, new Comparator<String>() {
-                public int compare (String s1, String s2) {
-                	int d=s2.length() - s1.length();
-                    
-                    if (d == 0) {
-                    	Integer c1 = formula.get(s1);
-                        Integer c2 = formula.get(s2);
-                        d = c1 - c2;
-                        if (d == 0)
-                            d = s1.compareTo(s2);
-                    }
-                    return d;
-                }
-            });
         StringBuilder sb = new StringBuilder ();
         for (String f : order) {
             Integer c = formula.get(f);
@@ -76,6 +54,6 @@ public class Chem {
             if (c > 1) sb.append(c);
             sb.append(f);
         }
-        return sb.toString();
+        return FormulaInfo.toCanonicalString(sb.toString());
     }
 }
---
+++ app/ix/core/chem/ElementInfo.java
@@ -0,0 +1,11 @@
+package ix.core.chem;
+
+public class ElementInfo {
+	public String symbol;
+	int count;
+
+	public ElementInfo(String s, int c) {
+		this.count = c;
+		this.symbol = s;
+	}
+}
