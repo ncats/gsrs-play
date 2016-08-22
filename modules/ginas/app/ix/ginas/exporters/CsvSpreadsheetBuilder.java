d0538d222b446a3c31e9bce4e95ec2eba07fcd06
katzelda <katzelda@mail.nih.gov>
2016-08-22 17:07:33 -0400

    added javadoc

--- modules/ginas/app/ix/ginas/exporters/CsvSpreadsheetBuilder.java
+++ modules/ginas/app/ix/ginas/exporters/CsvSpreadsheetBuilder.java
@@ -9,7 +9,8 @@ import java.util.Objects;
  * Created by katzelda on 8/22/16.
  */
 public class CsvSpreadsheetBuilder {
-    private static final SimpleDateFormat DEFAULT_FORMAT = new SimpleDateFormat("yyyy-MMM-dd");
+    public static final String DEFAULT_FORMAT_STRING = "yyyy-MMM-dd";
+    private static final SimpleDateFormat DEFAULT_FORMAT = new SimpleDateFormat(DEFAULT_FORMAT_STRING);
 
     private DateFormat dateFormat = new SimpleDateFormat(DEFAULT_FORMAT.toPattern());
     private String delimiter = ",";
@@ -19,6 +20,15 @@ public class CsvSpreadsheetBuilder {
 
     private int maxRowsInMemory =-1;
 
+    /**
+     * Create a new Builder that will build a {@link Spreadsheet} implementation
+     * that will write to the given output file.
+     * @param outputFile the File to write to.  If any parent directories
+     *                   do not exist, they will be created.
+     * @throws IOException if there is a problem creating parent directories or opening the file for writing.
+     *
+     * @throws NullPointerException if out is null.
+     */
     public CsvSpreadsheetBuilder(File outputFile) throws IOException {
         Objects.requireNonNull(outputFile);
 
@@ -31,13 +41,32 @@ public class CsvSpreadsheetBuilder {
         writer = new BufferedWriter(new FileWriter(outputFile));
     }
 
+    /**
+     * Create a new Builder that will build a {@link Spreadsheet} implementation
+     * that will write to the given outputStream.
+     * @param out the OutputStream to write to (can not be null).
+     *
+     * @throws NullPointerException if out is null.
+     */
     public CsvSpreadsheetBuilder(OutputStream out) {
         writer = new BufferedWriter(new OutputStreamWriter(out));
     }
 
+    /**
+     * Change the delimiter used to separate columns in the csv file
+     * (default is comma).
+     * @param delimiter the delimiter character to use.
+     * @return this
+     */
     public CsvSpreadsheetBuilder delimiter(char delimiter) {
         return delimiter(Character.toString(delimiter));
     }
+    /**
+     * Change the delimiter used to separate columns in the csv file
+     * (default is comma).
+     * @param delimiter the delimiter String to use.
+     * @return this
+     */
     public CsvSpreadsheetBuilder delimiter(String delimiter) {
         Objects.requireNonNull(delimiter);
         this.delimiter = delimiter;
@@ -45,6 +74,17 @@ public class CsvSpreadsheetBuilder {
         return this;
     }
 
+    /**
+     * Set the max number of rows that can be in memory at any one time.
+     * If the number of rows created exceeds this number, then the old rows
+     * will be written to the output and can no longer be modified.
+     * This essentially makes a low memory footprint streaming writer
+     * where users can go back and randomly access/edit the previous X rows.
+     *
+     * @param maxRowsInMemory the number of rows to keep in memory.  If set to null,
+     *                        then everything will be kept in memory (the default).
+     * @return this
+     */
     public CsvSpreadsheetBuilder maxRowsInMemory(Integer maxRowsInMemory){
         if(maxRowsInMemory ==null){
             this.maxRowsInMemory = -1;
@@ -57,6 +97,14 @@ public class CsvSpreadsheetBuilder {
         return this;
     }
 
+    /**
+     * Set the {@link DateFormat} used to write dates in the csv file.
+     * By default it is {@value DEFAULT_FORMAT_STRING}.
+     *
+     * @param dateFormat the date format to use; can not be null.
+     * @return this.
+     * @throws NullPointerException if dateFormat is null.
+     */
     public CsvSpreadsheetBuilder dateFormat(DateFormat dateFormat) {
         Objects.requireNonNull(dateFormat);
         this.dateFormat = dateFormat;
@@ -64,12 +112,23 @@ public class CsvSpreadsheetBuilder {
         return this;
     }
 
+    /**
+     * Should the cells in the csv get quoted.  If not called,
+     * defaults to false.
+     * @param quoteCells {@code true} if should be quoted; {@code false} otherwise.
+     * @return this.
+     */
     public CsvSpreadsheetBuilder quoteCells(boolean quoteCells) {
         this.quoteCells = quoteCells;
 
         return this;
     }
 
+    /**
+     * Use the current configuration to construct a new {@link Spreadsheet}
+     * instance.
+     * @return a new Spreadsheet instance will never be null.
+     */
     public Spreadsheet build() {
 
         if(maxRowsInMemory ==-1) {
