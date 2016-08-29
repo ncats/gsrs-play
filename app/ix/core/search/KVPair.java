4a50c9835b070887fe7d5562a5ad00ce8de24983
tyler <peryeata@mail.nih.gov>
2016-08-29 15:35:21 -0400

    gotta love eclipse

--- app/ix/core/search/KVPair.java
+++ app/ix/core/search/KVPair.java
@@ -1,7 +1,5 @@
 package ix.core.search;
 
-import ix.core.search.FieldBasedSearchAnalyzer.DefaultKVPair;
-import ix.core.search.FieldBasedSearchAnalyzer.KVPair;
 
 public interface KVPair{
 	public String getKey();
