e9cb0522e86437d5d7becdfdd2c7e95ed1a766c4
Peryea <peryeata@NCATS-1907788-M.lan>
2016-10-03 01:53:41 -0400

    added script to make flattened list of tests to run

---
+++ makeTestBat.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm runTests.bat
+echo Generating runTests.bat
+export command="echo \"sbt.bat -Dconfig.file=modules/ginas/conf/ginas.conf %ginas/testOnly {0}%\""
+./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf  "ginas/testOnly ix.test.ix.test.RunAllGinasTests"|grep ^sbt|sed 's/%/\"/g' >>runTests.bat
+
