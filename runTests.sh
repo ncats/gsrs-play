3782f094fba106baabc720443748a46959e052bd
Peryea <peryeata@NCATS-1907788-M.local>
2016-10-03 17:14:56 -0400

    updated test scripts

--- runTests.sh
+++ runTests.sh
@@ -3,5 +3,5 @@
 # Run tests
 #
 
-export command="./activator -Dtestconfig=conf/ginas.conf \"ginas/testOnly {0}\""
-#./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf  "ginas/testOnly ix.test.ix.test.RunAllGinasTests"
+export command="./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf \"ginas/testOnly {0}\""
+./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf  "ginas/testOnly ix.test.ix.test.RunAllGinasTests"
