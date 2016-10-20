87ceb17c67e9dd8e945b8e8ecd1554432135105c
geetha <geetha.mandava@nih.gov>
2016-10-20 15:10:35 -0400

    unit test code for secondary database down

--- modules/ginas/test/ix/AbstractGinasTest.java
+++ modules/ginas/test/ix/AbstractGinasTest.java
@@ -1,8 +1,10 @@
 package ix;
 
+import ix.core.util.ExpectFailureChecker;
 import org.junit.Rule;
 
 import ix.test.util.TestNamePrinter;
+import org.junit.rules.ExpectedException;
 
 /**
  * Abstract test class for doing common operations
@@ -15,5 +17,11 @@ import ix.test.util.TestNamePrinter;
 public abstract class AbstractGinasTest {
 	@Rule
 	public TestNamePrinter printer = new TestNamePrinter();
+
+	@Rule
+	public ExpectFailureChecker checker = new ExpectFailureChecker();
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 	
 }
