3d066e8b95cccd1f8e1cc51bc8c036049beb917e
Peryea <peryeata@NCATS-1907788-M.lan>
2016-10-24 09:33:32 -0400

    regen tests

--- runTests.sh
+++ runTests.sh
@@ -3,5 +3,6 @@
 # Run tests
 #
 
+
 export command="./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf \"ginas/testOnly {0}\""
 ./activator -Dconfig.file=modules/ginas/conf/ginas.conf -Dtestconfig=conf/ginas.conf  "ginas/testOnly ix.test.RunAllGinasTests"
