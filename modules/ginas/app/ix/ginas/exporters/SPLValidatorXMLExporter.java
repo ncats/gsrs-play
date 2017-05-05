921e64ced0445283198069d608f6babe1464008f
Tyler Peryea <tylerperyea@gmail.com>
2017-05-05 17:48:15 -0400

    Added export support for pt spl choice list

---
+++ modules/ginas/app/ix/ginas/exporters/SPLValidatorXMLExporter.java
@@ -0,0 +1,92 @@
+package ix.ginas.exporters;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Objects;
+
+import ix.ginas.models.v1.Substance;
+
+public class SPLValidatorXMLExporter implements Exporter<Substance> {
+	private final BufferedWriter out;
+	 
+	public SPLValidatorXMLExporter(OutputStream out){
+		 Objects.requireNonNull(out);
+	     this.out = new BufferedWriter(new OutputStreamWriter(out));
+	}
+	
+	
+	public SPLValidatorXMLExporter(File outputFile) throws IOException{
+        this(new BufferedOutputStream(new FileOutputStream(outputFile)));
+    }
+	
+	@Override
+	public void close() throws IOException {
+		out.close();
+	}
+
+	@Override
+	public void export(Substance obj) throws IOException {
+	
+		if(obj.isPublic() && obj.getApprovalID()!=null && !obj.isDeprecated()){
+		
+			String approvalID = obj.getApprovalIDDisplay();
+			String temp = "<choice><label>$Name</label><value>" +approvalID + "</value></choice>";
+			try{
+				obj.names.stream()
+			        .filter(n->!n.isDeprecated())
+			        .filter(n->n.isDisplayName() || n.preferred)
+			        .filter(n->n.isPublic())
+			        .map(n->n.getName())
+			        .map(n->temp.replace("$Name", encodeXML(n)))
+					.forEach(n->{
+						
+						try {
+							out.write(n);
+							out.write("\n");
+						} catch (IOException e) {
+							throw new IllegalStateException(e);
+						}
+					});
+			}catch(IllegalStateException e){
+				throw new IOException(e.getCause());
+			}
+		}
+		
+	}
+	
+	
+	static String[] lookup = new String[127];
+	
+	static{
+		for(int i=0; i< lookup.length; i++){
+			lookup[i] = "" +(char)i;
+		}
+		lookup['\"'] = "&quot";
+		lookup['<'] = "&lt;";
+		lookup['>'] = "&gt;";
+		lookup['&'] = "&amp;";
+		lookup['\''] = "&apos;";
+		
+	}
+	
+	private static String encodeXML(String t){
+		StringBuilder sb = new StringBuilder();
+		
+		
+		
+		for(int i = 0; i < t.length(); i++){
+		      char c = t.charAt(i);
+		      if(c>0x7e) {
+		    	  sb.append("&#"+((int)c)+";");
+		      }else{
+		    	  sb.append(lookup[c]);
+		      }
+		   }
+		return sb.toString();
+	}
+}
