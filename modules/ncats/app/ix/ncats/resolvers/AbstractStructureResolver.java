0b2d1939c1d94daf086f3353ac92bd38c3288ac8
Tyler Peryea <tylerperyea@gmail.com>
2017-10-10 00:26:32 -0400

    allow proxy for resolving structures

--- modules/ncats/app/ix/ncats/resolvers/AbstractStructureResolver.java
+++ modules/ncats/app/ix/ncats/resolvers/AbstractStructureResolver.java
@@ -1,13 +1,13 @@
 package ix.ncats.resolvers;
 
+import ix.core.util.ConfigHelper;
 import play.Logger;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.*;
+import java.util.Properties;
 
 import ix.core.models.Structure;
 import ix.core.chem.StructureProcessor;
@@ -21,6 +21,17 @@ public abstract class AbstractStructureResolver implements Resolver<Structure> {
     protected int maxtries = MAX_TRIES;
     protected int readTimeout = 5000; // read timeout 5s
     protected int connTimeout = 2000; // connect timeout 2s
+    public static final boolean PROXY_ENABLED = ConfigHelper.getBoolean("ix.proxy.enabled",false);
+    private static String PROXY_NAME;
+    private static int PORT_NUMBER;
+
+    //Static method to instantiate the parameter values for calling/connecting to the proxy server
+    static {
+        if (PROXY_ENABLED) {
+            PROXY_NAME = ConfigHelper.getOrDefault("ix.proxy.name","domain name");
+            PORT_NUMBER = ConfigHelper.getInt("ix.proxy.port",0);
+        }
+    }
 
     protected AbstractStructureResolver (String name) {
         if (name == null)
@@ -61,11 +72,24 @@ public abstract class AbstractStructureResolver implements Resolver<Structure> {
             for (int tries = 0; tries < maxtries; ++tries) {
                 try {
                     for (URL url : urls) {
-                        HttpURLConnection con =
-                            (HttpURLConnection)url.openConnection();
+
+                        // If the Proxy flag is enabled in the Config file, it connects to proxy or it wouldn't
+                        Proxy proxy = null;
+                        if(PROXY_ENABLED) {
+                            proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_NAME, PORT_NUMBER));
+                            System.out.println("Checking for Config file values");
+                            System.out.println("Proxy Name is :"+PROXY_NAME+"\n"+"Port Number is :"+PORT_NUMBER);
+                        }
+                        else
+                        {
+                            proxy = Proxy.NO_PROXY;
+                        }
+                        HttpURLConnection con = (HttpURLConnection)url.openConnection(proxy);
+                        con.connect();
+
                         con.setReadTimeout(readTimeout);
                         con.setConnectTimeout(connTimeout);
-                        
+
                         int status = con.getResponseCode();
                         Logger.debug("Resolving "+url+"..."+status);
                         if (status == HttpURLConnection.HTTP_NOT_FOUND)
