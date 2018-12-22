cd90fa46f8ba882b3358762c66e4dab03435dc6e
Mitch Miller <Mitch,Miller@thinkscience.us>
2018-12-21 23:54:45 -0500

    Resave back-ups for a particular type of object.

---
+++ modules/ginas/app/ix/ginas/initializers/ResaveBackupsTaskInitializer.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ix.ginas.initializers;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ix.core.initializers.Initializer;
+import ix.core.models.BaseModel;
+import ix.core.plugins.SchedulerPlugin.TaskListener;
+import ix.core.processors.BackupProcessor;
+import ix.core.util.EntityUtils;
+import ix.core.util.EntityUtils.EntityInfo;
+import ix.core.utils.executor.ProcessExecutionService;
+import ix.core.utils.executor.ProcessExecutionService.CommonStreamSuppliers;
+import ix.core.utils.executor.ProcessListener;
+
+/**
+ *
+ * @author Tyler
+ */
+public class ResaveBackupsTaskInitializer extends ScheduledTaskInitializer
+{
+
+	EntityInfo entityToResaveBackups = null;
+
+	@Override
+	public void run(TaskListener l)
+	{
+		resaveBackups(entityToResaveBackups.getEntityClass(), l);
+	}
+
+	private <T extends BaseModel> void resaveBackups(Class<T> cls, TaskListener l)
+	{
+		l.message("Initializing creating new backups");
+		ProcessListener listen = ProcessListener.onCountChange((sofar, total) ->
+		{
+			if (total != null)
+			{
+				l.message("Resaving backups:" + sofar + " of " + total);
+			} else
+			{
+				l.message("Resaving backups:" + sofar);
+			}
+		});
+
+		try
+		{
+			new ProcessExecutionService(5, 10).buildProcess(cls)
+					.streamSupplier(CommonStreamSuppliers.allFor(cls))
+					.consumer((T s) ->
+					{
+						BackupProcessor.getInstance()
+								.postUpdate(s);
+					})
+					.listener(listen)
+					.build()
+					.execute();
+		} catch (IOException ex)
+		{
+			Logger.getLogger(ResaveBackupsTaskInitializer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	@Override
+	public String getDescription()
+	{
+		return "Resave all backups of type " + entityToResaveBackups.getName() + " to the database backups.";
+	}
+
+	@Override
+	public Initializer initializeWith(Map<String, ?> m)
+	{
+		super.initializeWith(m);
+		String entityClass = (String) m.get("entityClass");
+		try
+		{
+			entityToResaveBackups = EntityUtils.getEntityInfoFor(entityClass);
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return this;
+	}
+
+}
