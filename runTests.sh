b0852b04d893ca403454031e03eadf037cf25218
katzelda <daniel.katzel@nih.gov>
2020-09-30 16:53:08 -0400

    added appending extra parameters to the runTests script

--- runTests.sh
+++ runTests.sh
@@ -4,5 +4,5 @@
 #
 
 
-export command="./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf \"ginas/testOnly {0}\""
-./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf  "ginas/testOnly ix.test.RunAllGinasTests"
+export command="./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf \"ginas/testOnly {0}\" $@"
+./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf  "ginas/testOnly ix.test.RunAllGinasTests" "$@"
