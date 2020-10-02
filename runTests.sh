46fa17430825e4f95edf801fe26328e299ebc36a
katzelda <daniel.katzel@nih.gov>
2020-10-02 13:56:10 -0400

    added the parse test result to run script

--- runTests.sh
+++ runTests.sh
@@ -5,4 +5,6 @@
 
 
 export command="./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf \"ginas/testOnly {0}\" $@"
-./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf  "ginas/testOnly ix.test.RunAllGinasTests" "$@"
+./activator -Dconfig.file=modules/ginas/conf/ginas-dev.conf  clean ginas/clean "ginas/testOnly ix.test.RunAllGinasTests" "$@"
+
+perl parseTestResults.pl modules/ginas/target/test-reports/
