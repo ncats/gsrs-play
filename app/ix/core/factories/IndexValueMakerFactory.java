7c32b5c80272123ef4499a65b071556d9655586a
katzelda <daniel.katzel@nih.gov>
2021-02-04 13:45:55 -0500

    added new `parameters` option to index value maker config

--- app/ix/core/factories/IndexValueMakerFactory.java
+++ app/ix/core/factories/IndexValueMakerFactory.java
@@ -1,13 +1,45 @@
 package ix.core.factories;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import ix.core.search.text.IndexValueMaker;
 import ix.core.search.text.ReflectingIndexValueMaker;
 import ix.core.util.EntityUtils;
 import ix.core.util.EntityUtils.EntityInfo;
+import ix.core.util.IOUtil;
 import play.Application;
 import play.Logger;
 
+import java.util.Collections;
+import java.util.Map;
+
 public class IndexValueMakerFactory extends AccumlatingInternalMapEntityResourceFactory<IndexValueMaker>{
+
+	public static class IndexValueMakerConfig{
+		public Class indexer;
+		@JsonProperty("class")
+		public Class entityType;
+
+		/**
+		 * Additional parameters to initialize in your instance returned
+		 * these will be used by Jackson to call the corresponding setters
+		 * using java beans method naming conventions.
+		 */
+		private Map<String, Object> parameters;
+	}
+	private static ThreadLocal<ObjectMapper> mapper = new ThreadLocal<ObjectMapper>(){
+		@Override
+		protected ObjectMapper initialValue() {
+			ObjectMapper mapper= new ObjectMapper();
+			TypeFactory tf = TypeFactory.defaultInstance()
+					.withClassLoader(IOUtil.getGinasClassLoader());
+			mapper.setTypeFactory(tf);
+
+			return mapper;
+
+		}
+	};
 	private static IndexValueMakerFactory _instance;
 	
 	public IndexValueMakerFactory(Application app) {
@@ -27,10 +59,15 @@ public class IndexValueMakerFactory extends AccumlatingInternalMapEntityResource
 		getStandardResourceStream(app,"ix.core.indexValueMakers")
 		.forEach(m->{
 			try{
-				EntityInfo<? extends IndexValueMaker> eiIndexer=
-						(EntityInfo<? extends IndexValueMaker>)
-						EntityUtils.getEntityInfoFor(m.get("indexer").toString());
-				register(m.get("class").toString(), (IndexValueMaker<?>)eiIndexer.getInstance(), true);
+				ObjectMapper mapperInstnce = mapper.get();
+				IndexValueMakerConfig conf = mapperInstnce.convertValue(m, IndexValueMakerConfig.class);
+				IndexValueMaker maker;
+				if(conf.parameters ==null){
+					maker = (IndexValueMaker) mapperInstnce.convertValue(Collections.emptyMap(), conf.indexer);
+				}else{
+					maker = (IndexValueMaker) mapperInstnce.convertValue(conf.parameters, conf.indexer);
+				}
+				register(conf.entityType, maker, true);
 			}catch(Exception e){
 				Logger.error("Unable to register IndexValueMaker:" + e.getMessage(), e);
 			}
