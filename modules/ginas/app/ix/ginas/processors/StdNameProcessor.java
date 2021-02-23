508f3626a410393de88522e9abaa3e4fcb40b414
epuzanov <egor.puzanov@bfarm.de>
2021-02-23 16:33:24 +0100

    do not change stdName automatically

---
+++ modules/ginas/app/ix/ginas/processors/StdNameProcessor.java
@@ -0,0 +1,18 @@
+package ix.ginas.processors;
+
+import ix.core.EntityProcessor;
+import ix.ginas.models.v1.Name;
+import ix.utils.Util;
+
+public class StdNameProcessor implements EntityProcessor<Name>{
+
+    @Override
+    public void prePersist(Name obj) {
+        obj.stdName = Util.getStringConverter().toStd(obj.getName());
+    }
+
+    @Override
+    public void preUpdate(Name obj) {
+        prePersist(obj);
+    }
+}
