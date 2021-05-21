68597c15bd23ea7c2234af8c72c4e93763f0f468
epuzanov <egor.puzanov@bfarm.de>
2021-05-21 09:16:07 +0200

    search JAVA_OPTS environment variable for config.file option in evolution.sh if running without args

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,16 +22,12 @@ do
     fi
 done
 
-if [ "x$GSRS_CACHE_RESET" = "xtrue" -o "x$GSRS_DB_RESET" = "xtrue" ]; then
-    echo "rm -rf ginas.ix/*"
+if [ "x$GSRS_CACHE_RESET" = "xtrue" ]; then
+    rm -rf ginas.ix/*
 fi
 
 if [ "x$GSRS_DB_RESET" = "xtrue" ]; then
-    CONFIG_FILE_OPT=`echo $JAVA_OPTS | grep -o "\-Dconfig.file *= *[^ ]*"`
-    if [ "x$CONFIG_FILE_OPT" = "x" ]; then
-        CONFIG_FILE_OPT="-Dconfig.file=conf/ginas.conf"
-    fi
-    java -cp "lib/*" $CONFIG_FILE_OPT ix.ginas.utils.Evolution
+    bin/evolutions.sh
 fi
 
 if [ -f "bin/build_extensions.sh" ]; then
--- evolutions.sh
+++ evolutions.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 ###################################################################
 
-#Script Name	:   evolutions.sh                                                                                           
-#Description	:   runs evolutions to create the schema needed for GSRS                                                                 
-#Args           :   $1 is the config file path, where the connections to
-#         			database are established                                                   
+#Script Name    :   evolutions.sh
+#Description    :   runs evolutions to create the schema needed for GSRS
+#Args           :   $1 is the config file path (optional), where the
+#                   connections to database are established
 ###################################################################
 
-java -cp "lib/*" -Dconfig.file=$1 ix.ginas.utils.Evolution
+if [ -z "$1" ]; then
+    CONFIG_FILE=`echo $JAVA_OPTS | grep -o "\-Dconfig.file=[^ ]*" | cut -d= -f2`
+else
+    CONFIG_FILE=$1
+fi
+
+if [ -f "$CONFIG_FILE" ]; then
+    java -cp "lib/*" -Dconfig.file=$CONFIG_FILE ix.ginas.utils.Evolution
+    if [ -d "ginas.ix" ]; then
+        rm -rf ginas.ix/*
+    fi
+fi
