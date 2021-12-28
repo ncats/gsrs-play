3fd22acd564cd2a13ec0849464c92637e5ca4bd3
NikoAnderson <nikolaus.g.anderson@gmail.com>
2021-12-28 15:53:57 -0500

    beta updates for 281

--- modules/ginas/conf/beta/assets/jsdraw/Scilligence.JSDraw2.Pro.js
+++ modules/ginas/conf/beta/assets/jsdraw/Scilligence.JSDraw2.Pro.js
@@ -25,13 +25,56 @@ JSDraw2.password = { encrypt: true, key: null, iv: null };
 // Place the license code below
 // Licensed to: FDA
 // Product: JSDraw
-// Expiration Date: 2020-Jul-01
-JSDraw2.licensecode='405565434916781761723242424242424131213141512181';
+// Expiration Date: 2022-Jul-30
+JSDraw2.licensecode='405562533916781761723242424242424131213141512181';
 
 
 
 //////////////////////////////////////////////////////////////////////////////////
 // JSDraw default settings
 
-JSDraw2.defaultoptions = {};
+JSDraw2.defaultoptions = {
+//Set the base URL for images and additional script loading
+"imagebase" : document.baseURI + "assets/jsdraw/"
+};
 
+
+//Added to try to pre-empt loading
+scil.Utils._scripturl=null; //this helps ensure temporary cached settings aren't honored
+scil.Utils._loadedAdditions=true; //flag that this extra file is loaded
+
+scil.Utils.imgSrc = function (button, wrapasinurl) {
+var s = null;
+if (button != null)
+    button = button.toLowerCase();
+
+var imgbase64 = (JSDraw2.Resources)?JSDraw2.Resources[button]:null;
+if (imgbase64 != null) {
+    var p = button.lastIndexOf('.');
+    var type = button.substr(p + 1);
+    s = 'data:image/' + type + ';base64,' + imgbase64;
+}
+else {
+    s = scil.Utils._imgBase() + button;
+}
+if (wrapasinurl)
+    s = 'url(' + s + ')';
+return s;
+};
+
+scil.App.imgSmall = function (button, wrapasurl) {
+if (button != null)
+    button = button.toLowerCase();
+var imgbase64 = (JSDraw2.Resources)?JSDraw2.Resources['small/' + button]:null;
+if (imgbase64 != null) {
+    var p = button.lastIndexOf('.');
+    var type = button.substr(p + 1);
+    s = 'data:image/' + type + ';base64,' + imgbase64;
+}
+else {
+    s = 'small/' + button;
+    if (wrapasurl)
+        s = 'url(' + s + ')';
+}
+return s;
+};
